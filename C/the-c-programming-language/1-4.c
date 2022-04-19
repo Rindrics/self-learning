704128e4fdcc7121ff70e12a443c00aece0e368a
Akira Hayashi <smxshxishxad@me.com>
2022-04-19 21:43:01 +0900

    :sparkles: Convert Celsius to Fahrenheit

--- C/the-c-programming-language/1-4.c
+++ C/the-c-programming-language/1-4.c
@@ -4,15 +4,15 @@ int main(void) {
   int lower, upper, step;
   float fahr, celsius;
 
-  lower = 0;
-  upper = 300;
-  step = 20;
+  lower = -18;
+  upper = 150;
+  step = 8;
 
-  fahr = lower;
-  printf("Fahrenheit Celsius\n");
-  while (fahr <= upper) {
-    celsius = (5.0/9.0) * (fahr-32.0);
-    printf("      %4.0f  %6.1f\n", fahr, celsius);
-    fahr = fahr + step;
+  celsius = lower;
+  printf("Celsius Fahrenheit\n");
+  while (celsius <= upper) {
+    fahr = celsius * (9.0/5.0) + 32.0;
+    printf(" %6.1f       %4.0f \n", celsius, fahr);
+    celsius = celsius + step;
   }
 }
