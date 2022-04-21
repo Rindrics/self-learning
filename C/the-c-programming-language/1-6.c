13b215a36e2e38f297251e0378bd0d46790d845d
Akira Hayashi <smxshxishxad@me.com>
2022-04-22 01:57:59 +0900

    :recycle: Give input text

--- C/the-c-programming-language/1-6.c
+++ C/the-c-programming-language/1-6.c
@@ -5,9 +5,11 @@ int main(void) {
   int space = 0;
   int tab = 0;
   int cr = 0;
+  char inputtext[] = "The C Programming language	foo bar baz";
 
-  printf("enter text:");
-  while ((c = getchar()) != EOF) {
+  printf("input text: %s\n", inputtext);
+  for (int i = 0; i < sizeof(inputtext); i++) {
+    c = inputtext[i];
 
     if (c == ' ')
       ++space;
@@ -18,6 +20,6 @@ int main(void) {
     if (c == '\r')
       ++cr;
 
-    printf("space: %d;   tab: %d;   CR: %d\n", space, tab, cr);
   }
+  printf("space: %d;   tab: %d;   CR: %d\n", space, tab, cr);
 }
