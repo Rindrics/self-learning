f4a14d8503c5dcb81326e7a4c84ae507723eb0ec
Akira Hayashi <smxshxishxad@me.com>
2022-12-30 17:14:34 +0900

    Add button with event listner

--- JavaScript/step-up-JavaScript/step0/main.js
+++ JavaScript/step-up-JavaScript/step0/main.js
@@ -1,3 +1,8 @@
 console.log("Hello from main.js");
 var element = document.getElementById('innerTest');
 element.innerHTML = '<strong>JavaScript</strong> で書きました';
+
+var buttonElm = document.getElementById('testButton');
+buttonElm.addEventListener('click', function() {
+    alert('ボタンが押されました');
+});
