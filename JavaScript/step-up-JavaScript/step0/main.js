876f3b1ef86f5ee363601f65fe10e1ad4fec8990
Akira Hayashi <smxshxishxad@me.com>
2022-12-30 17:41:26 +0900

    Change alert message by condition

--- JavaScript/step-up-JavaScript/step0/main.js
+++ JavaScript/step-up-JavaScript/step0/main.js
@@ -4,5 +4,12 @@ element.innerHTML = '<strong>JavaScript</strong> で書きました';
 
 var buttonElm = document.getElementById('testButton');
 buttonElm.addEventListener('click', function() {
-    alert('ボタンが押されました');
+    var numberElm = document.getElementById('number');
+    var val = numberElm.value;
+    var num = parseInt(val);
+    if (num % 2 == 0) {
+        alert('偶数です');
+    } else {
+        alert('奇数です');
+    }
 });
