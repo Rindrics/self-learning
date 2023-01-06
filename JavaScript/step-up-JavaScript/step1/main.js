dd94bc911ae376752f73a5f0ea9a3f5c41791843
Akira Hayashi <smxshxishxad@me.com>
2023-01-07 00:52:32 +0900

    :recycle: Use template literal

--- JavaScript/step-up-JavaScript/step1/main.js
+++ JavaScript/step-up-JavaScript/step1/main.js
@@ -2,7 +2,7 @@ function StopWatch(options) {
     function addMessage(message) {
         const messageElm = document.createElement('div');
         const now = new Date();
-        messageElm.innerText = now.getHours() + '時' + now.getMinutes() + '分' + now.getSeconds() + '秒 ' + message;
+        messageElm.innerText = `${now.getHours()}時${now.getMinutes()}分${now.getSeconds()}秒 ${message}`;
         messageElm.classList = ['message'];
         logElm.appendChild(messageElm);
     }
