c7043dc41a3776f3a12e47117c0cde00a1c0a416
Akira Hayashi <smxshxishxad@me.com>
2023-02-06 21:40:05 +0900

    :mega: Log quizData

--- JavaScript/step-up-JavaScript/quizgame/main.js
+++ JavaScript/step-up-JavaScript/quizgame/main.js
@@ -2,6 +2,12 @@ class WordQuiz {
     constructor() {
         console.log('instance created');
     }
+
+    async init() {
+        const response = await fetch('quiz.json');
+        this.quizData = await response.json();
+        console.log(this.quizData);
+    }
 }
 
-new WordQuiz();
+new WordQuiz().init();
