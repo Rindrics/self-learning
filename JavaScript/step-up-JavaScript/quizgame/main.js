1cdad8fe0225ec701076458432ac2e221f968008
Akira Hayashi <smxshxishxad@me.com>
2023-02-08 00:37:53 +0900

    :sparkles: Map quiz levels to dropdown menu items

--- JavaScript/step-up-JavaScript/quizgame/main.js
+++ JavaScript/step-up-JavaScript/quizgame/main.js
@@ -5,6 +5,7 @@ class WordQuiz {
 
     async init() {
         await this.fetchQuizData();
+        this.displayStartView();
     }
 
     async fetchQuizData() {
@@ -15,6 +16,23 @@ class WordQuiz {
             this.rootElm.innerText = 'Failed to load quiz';
             console.log(e);
         }
+    }
+
+    displayStartView() {
+        const levelStrs = Object.keys(this.quizData);
+        const optionStrs = [];
+        for (let i = 0; levelStrs.length > i; i++) {
+            optionStrs.push(`<option value="${levelStrs[i]}" name="level">レベル${i + 1}</option>`);
+        }
+        const html = `
+          <select class="levelSelector">
+            ${optionStrs.join('')}
+          </select>
+        `;
+        const parentElm = document.createElement('div');
+        parentElm.innerHTML = html;
+
+        this.rootElm.appendChild(parentElm);
     }
 }
 new WordQuiz(document.getElementById('app')).init();
