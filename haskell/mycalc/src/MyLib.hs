d5f3d7f2b600df6811a279f51e293a497fc70c7f
Akira Hayashi <akirahayashi@ah-R06-MBP.local>
2023-02-19 15:22:01 +0900

    :sparkles: Add `push`

--- haskell/mycalc/src/MyLib.hs
+++ haskell/mycalc/src/MyLib.hs
@@ -1,9 +1,16 @@
 module MyLib
 (
-  parseToken
+  parseToken,
+  push
 ) where
 
 parseToken :: String -> String -> (String, String)
 parseToken (' ':_) frag = (frag, "")
 parseToken [] frag = (frag, "")
 parseToken (x:xs) frag = parseToken xs (frag ++ [x])
+
+rpn :: String -> [String] -> [String]
+rpn x stack = push x stack
+
+push :: a -> [a] -> [a]
+push a b = a:b
--- haskell/mycalc/test/Main.hs
+++ haskell/mycalc/test/Main.hs
@@ -1,12 +1,15 @@
 import Test.Hspec (Spec, hspec, describe, it, shouldBe)
 import Test.Hspec.Expectations
-import MyLib (parseToken)
+import MyLib (parseToken, push)
 
 main :: IO ()
-main = hspec spec
+main = hspec $ do
 
-spec :: Spec
-spec = describe "parseToken works" $ do
-    it "returns lhs" $ fst (parseToken "1 2 +" "") `shouldBe` "1"
-    it "returns rhs" $ fst (parseToken "2 +" "") `shouldBe` "2"
-    it "returns operator" $ fst (parseToken "+" "") `shouldBe` "+"
+  describe "parseToken works" $ do
+      it "returns lhs" $ fst (parseToken "1 2 +" "") `shouldBe` "1"
+      it "returns rhs" $ fst (parseToken "2 +" "") `shouldBe` "2"
+      it "returns operator" $ fst (parseToken "+" "") `shouldBe` "+"
+
+  describe "stack" $ do
+      it "can push" $ push 1 [2, 3] `shouldBe` [1, 2, 3]
+      it "can push (start from empty)" $ push 1 [] `shouldBe` [1]
