15f03f0e6d96f319f658509b4a8bcedd55c2cf8c
Akira Hayashi <akirahayashi@ah-R06-MBP.local>
2023-02-19 15:41:42 +0900

    :sparkles: Add `pop`

--- haskell/mycalc/src/MyLib.hs
+++ haskell/mycalc/src/MyLib.hs
@@ -1,7 +1,8 @@
 module MyLib
 (
   parseToken,
-  push
+  push,
+  pop
 ) where
 
 parseToken :: String -> String -> (String, String)
@@ -9,8 +10,12 @@ parseToken (' ':_) frag = (frag, "")
 parseToken [] frag = (frag, "")
 parseToken (x:xs) frag = parseToken xs (frag ++ [x])
 
-rpn :: String -> [String] -> [String]
-rpn x stack = push x stack
+-- rpn :: String -> [String] -> [String]
+-- rpn x stack = push x stack
 
 push :: a -> [a] -> [a]
 push a b = a:b
+
+pop :: [a] -> Maybe (a, [a])
+pop [] = Nothing
+pop (x:xs) = Just (x, xs)
--- haskell/mycalc/test/Main.hs
+++ haskell/mycalc/test/Main.hs
@@ -1,6 +1,6 @@
 import Test.Hspec (Spec, hspec, describe, it, shouldBe)
 import Test.Hspec.Expectations
-import MyLib (parseToken, push)
+import MyLib (parseToken, push, pop)
 
 main :: IO ()
 main = hspec $ do
@@ -13,3 +13,6 @@ main = hspec $ do
   describe "stack" $ do
       it "can push" $ push 1 [2, 3] `shouldBe` [1, 2, 3]
       it "can push (start from empty)" $ push 1 [] `shouldBe` [1]
+      it "can pop (3 elements)" $ pop [1, 2, 3] `shouldBe` Just (1, [2, 3])
+      it "can pop (1 element)" $ pop [3] `shouldBe` Just (3, [])
+      it "can't pop (no element)" $ pop [] `shouldBe` (Nothing :: Maybe (Int, [Int]))
