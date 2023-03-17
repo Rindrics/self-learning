ba7bdb984e190ea041a0be088b0091ba9d2766cb
Akira Hayashi <smxshxishxad@me.com>
2023-03-18 06:31:28 +0900

    :sparkles: Calculate score

--- JavaScript/step-up-JavaScript/quizgame/main.js
+++ JavaScript/step-up-JavaScript/quizgame/main.js
@@ -48,6 +48,20 @@ class WordQuiz {
         console.log(`解答結果: ${answer}`);
     }
 
+    calcScore() {
+        let correctNum = 0;
+        const results = this.gameStatus.results;
+
+        for (const result of results) {
+            const selected = result.selectedAnswer;
+            const correct = result.question.answer;
+            if (selected === correct) {
+                correctNum++;
+            }
+        }
+        return Math.floor((correctNum / results.length) * 100);
+    }
+
     resetGame() {
         this.gameStatus.level = null;
         this.gameStatus.step = 1;
@@ -120,8 +134,11 @@ class WordQuiz {
     }
 
     displayResultView() {
+        const score = this.calcScore();
+
         const html = `
           <p>ゲーム終了</p>
+          <p>正解率: ${score}%</p>
           <button class="resetBtn">開始画面に戻る</button>
         `;
 
