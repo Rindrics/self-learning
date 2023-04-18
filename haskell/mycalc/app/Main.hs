8c4a32fb8055161c5e1c113e813aa06340a9c3d2
Akira Hayashi <akira.hayashi@route06.co.jp>
2023-04-19 01:08:54 +0900

    :recycle: Simplify function calculateRPN

--- haskell/mycalc/app/Main.hs
+++ haskell/mycalc/app/Main.hs
@@ -1,8 +1,8 @@
 module Main where
 
-import qualified MyLib (parseStringToTokenList, calculateRPN)
+import qualified MyLib (calculateRPN)
 
 main :: IO ()
 main = do
   line <- getLine
-  print $ MyLib.calculateRPN (reverse $ MyLib.parseStringToTokenList line [] []) []
+  print $ MyLib.calculateRPN line
--- haskell/mycalc/src/MyLib.hs
+++ haskell/mycalc/src/MyLib.hs
@@ -1,23 +1,11 @@
 module MyLib
 (
-  parseStringToTokenList,
   calculateRPN,
 ) where
 
-type StringToConsume = String
-type Accumulator = String
-type Token = String
-type Operand = Double
-
-parseStringToTokenList :: StringToConsume -> Accumulator -> [Token] -> [Token]
-parseStringToTokenList "" reversedToken tokenList = reverse reversedToken:tokenList
-parseStringToTokenList (' ':xs) reversedToken tokenList = parseStringToTokenList xs "" (reverse reversedToken:tokenList)
-parseStringToTokenList (x:xs) accumulator tokenList = parseStringToTokenList xs (x:accumulator) tokenList
-
-calculateRPN :: [Token] -> [Operand] -> Double
-calculateRPN [] result = head result
-calculateRPN ("+":rest) (num1:num2:numrest) = calculateRPN rest ((num2 + num1):numrest)
-calculateRPN ("-":rest) (num1:num2:numrest) = calculateRPN rest ((num2 - num1):numrest)
-calculateRPN ("*":rest) (num1:num2:numrest) = calculateRPN rest ((num2 * num1):numrest)
-calculateRPN ("/":rest) (num1:num2:numrest) = calculateRPN rest ((num2 / num1):numrest)
-calculateRPN (x:xs) stack = calculateRPN xs ((read x :: Double):stack)
+calculateRPN :: String -> Double
+calculateRPN = head . foldl foldingFunction [] . words
+  where foldingFunction (x:y:ys) "*" = (y * x):ys
+        foldingFunction (x:y:ys) "+" = (y + x):ys
+        foldingFunction (x:y:ys) "-" = (y - x):ys
+        foldingFunction xs numberString = read numberString:xs
