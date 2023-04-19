8e73187226eea9f42bd53648616b6e46d59be9bc
Akira Hayashi <akirahayashi@ah-R06-MBP.local>
2023-04-19 15:14:26 +0900

    :sparkles: Print `RoadSystem` in main

--- haskell/shortest-route/app/Main.hs
+++ haskell/shortest-route/app/Main.hs
@@ -1,6 +1,13 @@
 module Main (main) where
 
-import Lib
+import Lib (RoadSystem, Section(..))
 
 main :: IO ()
-main = someFunc
+main = do
+  let heathrowToLondon :: RoadSystem
+      heathrowToLondon = [ Section 50 10 30
+                         , Section 5 90 20
+                         , Section 40 2 25
+                         , Section 10 8 0
+                         ]
+  print heathrowToLondon
