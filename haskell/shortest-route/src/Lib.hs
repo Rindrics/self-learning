2d42cea5e765d41e9b379b8de6d9455344ef6285
Akira Hayashi <akirahayashi@ah-R06-MBP.local>
2023-04-19 14:38:27 +0900

    :sparkles: Add new type `RoadSystem` to hold `Section`s

--- haskell/shortest-route/src/Lib.hs
+++ haskell/shortest-route/src/Lib.hs
@@ -1,10 +1,13 @@
 module Lib
     ( someFunc
     , Section(..)
+    ,  RoadSystem
     ) where
 
 data Section = Section { getA :: Int, getB :: Int, getC :: Int}
   deriving (Show)
+
+type RoadSystem = [Section]
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
