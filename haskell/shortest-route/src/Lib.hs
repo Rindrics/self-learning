8d532e0039bbcc3c1ae191605b5ade87328693d8
Akira Hayashi <akirahayashi@ah-R06-MBP.local>
2023-04-19 14:37:31 +0900

    :sparkles: Define data `Section` to store triplet integer

--- haskell/shortest-route/src/Lib.hs
+++ haskell/shortest-route/src/Lib.hs
@@ -1,6 +1,10 @@
 module Lib
     ( someFunc
+    , Section(..)
     ) where
+
+data Section = Section { getA :: Int, getB :: Int, getC :: Int}
+  deriving (Show)
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
