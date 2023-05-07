1bc76ab32ca3ac3a822b6c3a5b85f6ba8c977620
Akira Hayashi <smxshxishxad@me.com>
2023-05-08 07:35:25 +0900

    :sparkles: Define `optimalPath`

--- haskell/shortest-route/src/Lib.hs
+++ haskell/shortest-route/src/Lib.hs
@@ -1,6 +1,7 @@
 module Lib
     (  Section(..)
     ,  RoadSystem
+    ,  optimalPath
     ,  roadStep
     ) where
 
@@ -12,7 +13,12 @@ type RoadSystem = [Section]
 data Label = A | B | C deriving (Show)
 type Path = [(Label, Int)]
 
--- optimalPath :: RoadSystem -> Path
+optimalPath :: RoadSystem -> Path
+optimalPath roadSystem =
+  let (bestAPath, bestBPath) = foldl roadStep ([], []) roadSystem
+  in  if sum (map snd bestAPath) <= sum (map snd bestBPath)
+        then reverse bestAPath
+        else reverse bestBPath
 
 roadStep :: (Path, Path) -> Section -> (Path, Path)
 roadStep (pathA, pathB) (Section a b c) =
