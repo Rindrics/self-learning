e5da370656a6e64ac3bd408706893c3d4854df47
Akira Hayashi <smxshxishxad@me.com>
2023-05-17 08:25:32 +0900

    :wrench: Update `Main.hs`

--- haskell/shortest-route/app/Main.hs
+++ haskell/shortest-route/app/Main.hs
@@ -1,13 +1,14 @@
 module Main (main) where
 
-import Lib (RoadSystem, Section(..), roadStep)
+import Lib (RoadSystem, Section(..), roadStep, groupsOf, optimalPath)
 
 main :: IO ()
 main = do
-  let heathrowToLondon :: RoadSystem
-      heathrowToLondon = [ Section 50 10 30
-                         , Section 5 90 20
-                         , Section 40 2 25
-                         , Section 10 8 0
-                         ]
-  print (roadStep ([], []) (head heathrowToLondon))
+  contents <- getContents
+  let threes = groupsOf 3 (map read $ lines contents)
+      roadSystem = map (\[a, b, c] -> Section a b c) threes
+      path = optimalPath roadSystem
+      pathString = concat $ map (show . fst) path
+      pathTime = sum $ map snd path
+  putStrLn $ "The best path to take is: " ++ pathString
+  putStrLn $ "Time taken: " ++ show pathTime
