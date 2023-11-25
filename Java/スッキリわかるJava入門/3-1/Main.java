9df5d37cb64d0455d4008d1623faccdddab626e3
Akira Hayashi <smxshxishxad@me.com>
2023-11-26 07:54:45 +0900

    Confirm logics in 3-1

--- Java/スッキリわかるJava入門/3-1/Main.java
+++ Java/スッキリわかるJava入門/3-1/Main.java
@@ -1,13 +1,13 @@
 class Main {
     public static void main(String[] args) {
-       int weight = 60;
+       int weight = 63;
          if (weight == 60) {
              System.out.format("weight (%d) は 60 です\n", weight);
          } else {
              System.out.format("weight (%d) は 60 ではありません\n", weight);
          }
 
-       int age1 = 20;
+       int age1 = 40;
        int age2 = 30;
        if ((age1 + age2) > 60) {
            System.out.format("合計年齢 (%d) は60を超えています\n", age1 + age2);
@@ -15,7 +15,7 @@ class Main {
            System.out.format("合計年齢 (%d) は60以下です\n", age1 + age2);
        }
 
-       int age = 22;
+       int age = 21;
        if ((age % 2) == 0) {
            System.out.format("age (%d) は偶数です\n", age);
        } else {
