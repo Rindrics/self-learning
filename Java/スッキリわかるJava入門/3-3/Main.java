12a7d51e87e584e587c5563f2cd0ebe36a19b8bd
Akira Hayashi <smxshxishxad@me.com>
2023-11-26 08:13:35 +0900

    Solve 3-3

---
+++ Java/スッキリわかるJava入門/3-3/Main.java
@@ -0,0 +1,17 @@
+class Main {
+    public static void main(String[] args) {
+        int isHungry = 0;
+        String food = "おにぎり";
+
+        System.out.println("こんにちは");
+
+        if (isHungry == 0) {
+            System.out.println("はらぺこです");
+            System.out.println(food + "をいただきます");
+        } else {
+            System.out.println("お腹がいっぱいです");
+        }
+
+        System.out.println("ごちそうさまでした");
+    }
+}
