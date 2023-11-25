92f7c3a53b2ae374c3c1568b48ef46bf5c1a3331
Akira Hayashi <smxshxishxad@me.com>
2023-11-26 07:54:08 +0900

    Solve 3-1

---
+++ Java/スッキリわかるJava入門/3-1/Main.java
@@ -0,0 +1,32 @@
+class Main {
+    public static void main(String[] args) {
+       int weight = 60;
+         if (weight == 60) {
+             System.out.format("weight (%d) は 60 です\n", weight);
+         } else {
+             System.out.format("weight (%d) は 60 ではありません\n", weight);
+         }
+
+       int age1 = 20;
+       int age2 = 30;
+       if ((age1 + age2) > 60) {
+           System.out.format("合計年齢 (%d) は60を超えています\n", age1 + age2);
+       } else {
+           System.out.format("合計年齢 (%d) は60以下です\n", age1 + age2);
+       }
+
+       int age = 22;
+       if ((age % 2) == 0) {
+           System.out.format("age (%d) は偶数です\n", age);
+       } else {
+           System.out.format("age (%d) は奇数です\n", age);
+       }
+
+       String name = "湊";
+       if (name.equals("湊")) {
+           System.out.format("name (%s) は湊です\n", name);
+       } else {
+           System.out.format("name (%s) は湊ではありません\n", name);
+       }
+    }
+}
