051dec858aa053e729a923cd308af6cc2512a07e
Akira Hayashi <smxshxishxad@me.com>
2023-11-26 07:22:16 +0900

    Solve 1-3

---
+++ Java/スッキリわかるJava入門/1-3/Main.java
@@ -0,0 +1,14 @@
+class Main {
+    public static void main(String[] args) {
+        boolean result = true;
+        char name = '駆';
+        float pi = 3.14F;
+        long number = 314159265853979L;
+        String msg = "ミナトの攻撃！敵に15ポイントのダメージを与えた。";
+        System.out.format("result: %b\n", result);
+        System.out.format("name: %s\n", name);
+        System.out.format("pi: %f\n", pi);
+        System.out.format("number: %d\n", number);
+        System.out.format("msg: %s\n", msg);
+    }
+}
