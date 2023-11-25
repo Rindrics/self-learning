d4e6eace4a2ed4dcc987a8e3799678189d58f9e9
Akira Hayashi <smxshxishxad@me.com>
2023-11-26 07:25:52 +0900

    Solve 2-1

---
+++ Java/スッキリわかるJava入門/2-1/Main.java
@@ -0,0 +1,8 @@
+class Main {
+    public static void main(String[] args){
+        int x = 5;
+        int y = 10;
+        String ans = "x + y は " + (x + y);
+        System.out.println(ans);
+    }
+}
