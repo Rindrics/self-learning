dc7f4343ddb80e348dbbbb7aa8b347828fe81dee
Akira Hayashi <smxshxishxad@me.com>
2023-11-26 10:52:12 +0900

    Avoid resource leak due to `Scanner()`

--- Java/スッキリわかるJava入門/3-5/Main.java
+++ Java/スッキリわかるJava入門/3-5/Main.java
@@ -1,24 +1,27 @@
 class Main {
     public static void main(String[] args) {
-       System.out.print("[メニュー] 1:検索 2:登録 3:削除 4:変更>");
-       int selected = new java.util.Scanner(System.in).nextInt();
-       String action;
-       switch (selected) {
-       case 1:
-           action = "検索";
-           // FALLTHROUGH
-       case 2:
-           action = "登録";
-           // FALLTHROUGH
-       case 3:
-           action = "削除";
-           // FALLTHROUGH
-       case 4:
-           action = "変更";
-           System.out.format("%sします\n", action);
-           break;
-       default:
-           break;
+        System.out.print("[メニュー] 1:検索 2:登録 3:削除 4:変更>");
+       try (java.util.Scanner scanner = new java.util.Scanner(System.in)) {
+           int selected = scanner.nextInt();
+
+           String action;
+           switch (selected) {
+           case 1:
+               action = "検索";
+               // FALLTHROUGH
+           case 2:
+               action = "登録";
+               // FALLTHROUGH
+           case 3:
+               action = "削除";
+               // FALLTHROUGH
+           case 4:
+               action = "変更";
+               System.out.format("%sします\n", action);
+               break;
+           default:
+               break;
+           }
        }
     }
 }
