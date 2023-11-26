ae3c6d486bffba47f80900c991df578bfa275663
Akira Hayashi <smxshxishxad@me.com>
2023-11-26 21:38:31 +0900

    Solve 5-1

---
+++ Java/スッキリわかるJava入門/5-1/Main.java
@@ -0,0 +1,17 @@
+class Main {
+    public static void main(String[] args) {
+        introduceOneself();
+    }
+
+    public static void introduceOneself() {
+        String name = "John";
+        int age = 20;
+        double height = 170.0;
+        char zodiac = '子';
+
+        System.out.println("私の名前は" + name + "です");
+        System.out.println("年齢は" + age + "です");
+        System.out.println("身長は" + height + "です");
+        System.out.println("干支は" + zodiac + "です");
+    }
+}
