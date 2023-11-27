6c0a5d022b86a5c1d6626b2b61187d5f35481730
Akira Hayashi <smxshxishxad@me.com>
2023-11-28 07:17:10 +0900

    Fix typo

--- Java/スッキリわかるJava入門/6-1/Zenhan.java
+++ Java/スッキリわかるJava入門/6-1/Zenhan.java
@@ -5,7 +5,7 @@ public class Zenhan {
         System.out.println("きなこでござる。食えませんがの。");
     }
 
-    public static void doTomage() {
+    public static void doTogame() {
         System.out.println("この老いぼれの目はごまかせませんぞ。");
     }
 }
