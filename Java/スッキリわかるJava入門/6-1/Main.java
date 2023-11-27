9348ac193a60229587239474ae73ff5cca158667
Akira Hayashi <smxshxishxad@me.com>
2023-11-28 07:08:50 +0900

    Move `callDeae()` and `showMondokoro()` to `comment.Kouhan`

--- Java/スッキリわかるJava入門/6-1/Main.java
+++ Java/スッキリわかるJava入門/6-1/Main.java
@@ -4,17 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Zenhan.doWarusa();
         Zenhan.doTogame();
-        callDeae();
-        showMondokoro();
-    }
-
-    public static void callDeae() {
-        System.out.println("えぇい、こしゃくな。くせ者だ！であえい！");
-    }
-
-    public static void showMondokoro() {
-        System.out.println("飛車さん、角さん。もういいでしょう。");
-        System.out.println("この紋所が目にはいらぬか！");
-        doTogame();
+        comment.Kouhan.callDeae();
+        comment.Kouhan.showMondokoro();
     }
 }
