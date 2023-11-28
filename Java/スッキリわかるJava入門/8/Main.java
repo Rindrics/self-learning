1a6778fca38487941fa8c025386dd55859547792
Akira Hayashi <smxshxishxad@me.com>
2023-11-29 06:32:31 +0900

    Call Matango from Main

--- Java/スッキリわかるJava入門/8/Main.java
+++ Java/スッキリわかるJava入門/8/Main.java
@@ -5,6 +5,14 @@ public class Main {
         h.hp = 100;
         System.out.println("勇者" + h.name + "を生み出しました！");
 
+        Matango m1 = new Matango();
+        m1.hp = 50;
+        m1.suffix = 'A';
+
+        Matango m2 = new Matango();
+        m2.hp = 48;
+        m2.suffix = 'B';
+
         h.sit(5);
         h.slip();
         h.sit(25);
