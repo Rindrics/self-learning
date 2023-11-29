6e343c70c23901d630ac941e8de7fe03b8ae15bd
Akira Hayashi <smxshxishxad@me.com>
2023-11-30 07:20:03 +0900

    Solve 9-1

--- Java/スッキリわかるJava入門/9/Thief.java
+++ Java/スッキリわかるJava入門/9/Thief.java
@@ -2,4 +2,14 @@ public class Thief {
     String name;
     int hp;
     int mp;
+
+    public Thief(String name, int hp, int mp) {
+        this.name = name;
+        this.hp = hp;
+        this.mp = mp;
+    }
+
+    public Thief(String name) {
+        this(name, 40, 5);
+    }
 }
