16aba568eae5aec7c0e41ea35476698068bd20d0
Akira Hayashi <smxshxishxad@me.com>
2023-12-03 15:51:32 +0900

    Solve 11-1 (Extend `Book` and `Computer` from `TangibleAsset`)

--- Java/スッキリわかるJava入門/11/Book.java
+++ Java/スッキリわかるJava入門/11/Book.java
@@ -1,21 +1,10 @@
-public class Book {
-    string name;
-    int price;
-    String color;
+public class Book extends TangibleAsset {
     String isbn;
 
     public Book (String name, int price, String color, String isbn) {
-        this.name = name;
-        this.price = price;
-        this.color = color;
+        super(name, price, color);
         this.isbn = isbn;
     }
-
-    public String getName() {return this.name;}
-
-    public int getPrice() {return this.price;}
-
-    public String getColor() {return this.color;}
 
     public String getIsbn() {return this.isbn;}
 }
