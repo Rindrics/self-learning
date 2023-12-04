17a43b06f9c1d46542cbc45d3210069eea86c5b6
Akira Hayashi <smxshxishxad@me.com>
2023-12-05 07:18:33 +0900

    Solve 12-3

--- Java/スッキリわかるJava入門/12/Main.java
+++ Java/スッキリわかるJava入門/12/Main.java
@@ -8,5 +8,16 @@ public class Main {
 
        y1.a();
        y2.a();
+
+       Y[] array = new Y[2];
+       array[0] = y1;
+       array[1] = y2;
+
+       System.out.println("before loop");
+       for (Y y : array) {
+           System.out.println("inside loop");
+           y.b();
+       }
+       System.out.println("after loop");
     }
 }
