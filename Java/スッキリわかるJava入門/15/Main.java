cf673cc8483ac83f52a6a3fce27a18ce6c215bda
Akira Hayashi <smxshxishxad@me.com>
2023-12-10 07:05:58 +0900

    Solve 15-3-1

--- Java/スッキリわかるJava入門/15/Main.java
+++ Java/スッキリわかるJava入門/15/Main.java
@@ -1,4 +1,6 @@
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,10 +13,22 @@ public class Main {
 
         String[] numbers = sb.toString().split(",");
         System.out.println(Arrays.toString(numbers));
+
+        String mydata = "some string with 'the data i want' inside";
+        MatchString(mydata);
     }
 
     public String ConstructFiledir(String folder, String file) {
         String folderWithoutTrailingSlash = folder.replaceAll("/$", "");
         return folderWithoutTrailingSlash + "/" + file;
+    }
+
+    public static void MatchString(String str) {
+        Pattern all = Pattern.compile(".*");
+        Matcher matcherAll = all.matcher(str);
+        if (matcherAll.find())
+        {
+            System.out.println(matcherAll.group(0));
+        }
     }
 }
