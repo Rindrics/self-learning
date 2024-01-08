2784496adba19cb88e3d3fda6511172525a76869
Akira Hayashi <smxshxishxad@me.com>
2024-01-09 07:44:24 +0900

    Get page uisng SWR to render page freuquently

--- webapp/hands-on-full-stack/frontend/app/hello_frontend_swr/page.tsx
+++ webapp/hands-on-full-stack/frontend/app/hello_frontend_swr/page.tsx
@@ -1,18 +1,14 @@
 'use client'
 
-import axios from 'axios'
-import { useEffect, useState } from 'react'
+import useSWR from 'swr'
+
+const fetcher = (url: string) => fetch(url).then(res => res.json())
 
 export default function Page() {
-  const [data, setData] = useState({ name: '' })
+  const { data, error } = useSWR('/api/hello', fetcher)
 
-  useEffect(() => {
-    axios.get('/api/hello')
-      .then((res) => res.data)
-      .then((data) => {
-        setData(data)
-      })
-  }, [])
+  if (error) return <div>failed to load</div>
+  if (!data) return <div>loading...</div>
 
   return <div>hello {data.name}!</div>
 }
